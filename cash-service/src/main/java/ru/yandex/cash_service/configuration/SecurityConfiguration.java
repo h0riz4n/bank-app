9a9317ed0259f1b45746d7cf86f968d2e82d166d
Rustam Khudoyan <dreamang2toplay@yandex.ru>
2026-02-21 23:37:35 +0300

    feat: Добавлен сервис по работе с пополнением и снятие баланса

--- cash-service/src/main/java/ru/yandex/cash_service/configuration/SecurityConfiguration.java
+++ cash-service/src/main/java/ru/yandex/cash_service/configuration/SecurityConfiguration.java
@@ -1,5 +1,56 @@
 package ru.yandex.cash_service.configuration;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .csrf(AbstractHttpConfigurer::disable)
+            .cors(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(authz -> {
+                authz
+                    .requestMatchers("/api/**").hasAuthority("user")
+                    .anyRequest().permitAll();
+            })
+            .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {
+                JwtAuthenticationConverter jwtAuthConverter = new JwtAuthenticationConverter();
+                jwtAuthConverter.setJwtGrantedAuthoritiesConverter(this::toGrantedAuthorities);
+                jwt.jwtAuthenticationConverter(jwtAuthConverter);
+            }))
+            .sessionManagement(sessionManagment -> sessionManagment.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .build();
+    }
+
+    private Collection<GrantedAuthority> toGrantedAuthorities(Jwt jwt) {
+        Map<String, List<String>> realmAccess = jwt.getClaim("realm_access");
+        return realmAccess
+            .get("roles")
+            .stream()
+            .map(SimpleGrantedAuthority::new)
+            .collect(Collectors.toList());
+    }
 }
---
+++ cash-service/src/main/java/ru/yandex/cash_service/handler/RestControllerHandler.java
@@ -0,0 +1,108 @@
+package ru.yandex.cash_service.handler;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class RestControllerHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<ProblemDetail> handleRuntimeException(RuntimeException e) {
+        log.error("Unhandled runtime exception ({}) handler: {}", e.getClass().getSimpleName(), e.getMessage(), e);
+        return ResponseEntity
+            .of(buildProblemDetail("Unhandled API error", HttpStatus.INTERNAL_SERVER_ERROR))
+            .build();
+    }
+
+    @ExceptionHandler(HttpClientErrorException.class)
+    public ResponseEntity<ProblemDetail> handleError4xxException(HttpClientErrorException e) {
+        return ResponseEntity
+            .of(e.getResponseBodyAs(ProblemDetail.class))
+            .build();
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ProblemDetail> handleConstraintViolationException(ConstraintViolationException e) {
+        Map<String, String> errorMap = new HashMap<>();
+        e.getConstraintViolations().forEach(error -> errorMap.put(error.getPropertyPath().toString(), error.getMessage()));
+        log.debug("ConstraintViolationException handler: {}", e.getConstraintViolations().stream().map(error -> "\n%s - %s".formatted(error.getPropertyPath().toString(), error.getMessage())).collect(Collectors.joining("; ")));
+        return ResponseEntity
+            .of(buildProblemDetail("Invalid request body", HttpStatus.BAD_REQUEST, Map.of("validation", errorMap)))
+            .build();
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ProblemDetail> handleAccessDeniedException(AccessDeniedException e) {
+        log.debug("AccessDeniedException handler: {}", e.getMessage());
+        return ResponseEntity
+            .of(buildProblemDetail(e.getMessage(), HttpStatus.FORBIDDEN))
+            .build();
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity<ProblemDetail> handleAuthenticationException(AuthenticationException e) {
+        log.debug("AuthenticationException: {}", e.getMessage());
+        return ResponseEntity
+            .of(buildProblemDetail(e.getMessage(), HttpStatus.UNAUTHORIZED))
+            .build();
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String, String> errorMap = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> errorMap.put(((FieldError) error).getField(), error.getDefaultMessage()));
+        ProblemDetail body = buildProblemDetail("Invalid request parameters", HttpStatus.BAD_REQUEST, Map.of("validation", errorMap));
+        return this.handleExceptionInternal(ex, body, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
+        log.debug("{} handler: {}; path: {}", ex.getClass().getSimpleName(), ex.getMessage());
+        return super.handleExceptionInternal(ex, body, headers, statusCode, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+      return this.handleExceptionInternal(ex, (Object)null, headers, status, request);
+   }
+
+    private ProblemDetail buildProblemDetail(String message, HttpStatusCode status) {
+        return buildProblemDetail(message, status, null);
+    }
+
+    private ProblemDetail buildProblemDetail(String message, HttpStatusCode status, Map<String, Object> properties) {
+        URI instance = URI.create(((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest().getRequestURI());
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(status, message);
+        problemDetail.setInstance(instance);
+        problemDetail.setProperties(properties);
+        return problemDetail;
+    }
+}
